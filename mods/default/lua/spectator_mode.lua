d17ce0dfa8340848097ecbdbcb7dd309bab03117
LNJ2 <lnj.git@gmail.com>
2016-05-27 14:03:51 +0200

    Make everything GPLv3+ that is compatible.

--- mods/default/lua/spectator_mode.lua
+++ mods/default/lua/spectator_mode.lua
@@ -1,7 +1,27 @@
--- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
---  This file is from kilbith's WTFPL mod: Spectator Mode
---  See: https://github.com/minetest-mods/spectator_mode/ for details.
--- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+--[[
+License of this file:
+---------------------
+Copyright (C) 2016 kilbith <jeanpatrick.guerrero@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+Note:
+-----
+This file is originallly from kilbith's mod: Spectator Mode!
+See: https://github.com/minetest-mods/spectator_mode/ for details.
+--]]
 
 local original_pos = {}
 
